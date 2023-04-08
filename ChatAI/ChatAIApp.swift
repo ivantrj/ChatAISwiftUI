38ce61c4cea2e5b73c294cdd180f97730fc0cdbc
Ivan Trj <ivantrajanovski@gmail.com>
2023-04-08 11:53:33 +0200

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by Ivan Trajanovski on 08.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
